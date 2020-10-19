f313d2cb57e0913089d5a4794f1c29455a4e97c0
andrewcreates <wer345erty@gmail.com>
2020-10-19 16:42:20 -0700

    added solutions for Q2 and Q3 of Twitch interview

--- scripts/minutes_streamed table and generated data.sql
+++ scripts/minutes_streamed table and generated data.sql
@@ -166,3 +166,49 @@ INSERT INTO minutes_streamed (time_minute, streamer_username, category, concurre
 					FLOOR(avg_concurrent + (4*RANDOM() - 2) * std_dev_concurrent)
 			FROM series
 			WHERE streamer_username = 'Gon' AND category = 'HXH') input
+
+
+Question 2: Write a query that returns a row for each streamer with columns for their total hours streamed (in any category) and percentage of hours streamed in a Call of Duty game category.
+
+WITH hours_streamed AS(
+	SELECT streamer_username, 
+		COUNT(*)/60 AS tot_hours_streamed, 
+		(COUNT(*) FILTER(WHERE INITCAP(category) LIKE 'Call Of Duty%'))/60 AS COD_hours_streamed
+	FROM minutes_streamed
+	GROUP BY streamer_username
+	)
+	
+SELECT streamer_username,
+	tot_hours_streamed,
+	to_char(COD_hours_streamed::numeric/tot_hours_streamed*100,'990D99%') AS perc_COD
+FROM hours_streamed
+GROUP BY streamer_username, COD_hours_streamed, tot_hours_streamed
+
+Question 3: For each calendar month, output the list of streamers who increased their hours streamed from the previous calendar month
+
+WITH names AS (
+		SELECT DISTINCT streamer_username AS name
+		FROM minutes_streamed
+	),
+	months AS(
+		SELECT GENERATE_SERIES(MIN(DATE_TRUNC('month', time_minute)) - '1 month'::INTERVAL, MAX(DATE_TRUNC('month', time_minute)), '1 month'::interval) AS month,
+		name
+		FROM minutes_streamed
+		CROSS JOIN names
+		GROUP BY name
+	),
+	hours_streamed AS(
+		SELECT m.month, m.name, 
+			COUNT(*)/60 AS tot_hours_streamed, 
+			LAG(COUNT(*)/60, 1) OVER(PARTITION BY m.name ORDER BY m.month) AS prev_months_hours
+		FROM months m
+		LEFT OUTER JOIN minutes_streamed ms ON DATE_TRUNC('month', ms.time_minute) = m.month AND ms.streamer_username = m.name
+		GROUP BY m.month, m.name
+		ORDER BY m.month
+	)
+
+SELECT m.month, string_agg(h.name, ', ') FILTER (WHERE h.prev_months_hours < h. tot_hours_streamed) AS streamer_list
+FROM hours_streamed h
+RIGHT OUTER JOIN months m ON m.month = h.month AND m.name = h.name
+GROUP BY m.month
+ORDER BY m.month
