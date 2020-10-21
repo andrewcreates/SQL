8dc6ca29ff8757cd68d09d4419e758880ad5faaf
andrewcreates <wer345erty@gmail.com>
2020-10-21 10:32:28 -0700

    Updated table constraint for orders to allow DELETE CASCADE

--- databases/Northwind2016.sql
+++ databases/Northwind2016.sql
@@ -3839,7 +3839,7 @@ ALTER TABLE ONLY order_details
 --
 
 ALTER TABLE ONLY order_details
-    ADD CONSTRAINT fk_order_details_orders FOREIGN KEY (order_id) REFERENCES orders;
+    ADD CONSTRAINT fk_order_details_orders FOREIGN KEY (order_id) REFERENCES orders ON DELETE CASCADE;
 
 
 --
